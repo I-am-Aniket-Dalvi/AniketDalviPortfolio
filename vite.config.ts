7416f4ff5e38a4d3d96a39f3e13e5a43da33b8c5
I-am-Aniket-Dalvi <aniketd741@gmail.com>
2025-10-30 15:31:24 +0530

    rebuild for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/AniketDalviPortfolio/", // 👈 हे नेमकं repo name match हवं
   server: {
     host: "::",
     port: 8080,
